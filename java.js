9beb3009facf6d8e8d3178efc8204cac2b6cfb4a
MirandaLee1 <134345648+MirandaLee1@users.noreply.github.com>
2023-10-23 12:02:52 +0200

    java updates

--- java.js
+++ java.js
@@ -1,40 +1,16 @@
-let now = new Date();
+function displayTemperature(response) {
 
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-  "Sunday",
-];
 
-let day = days[now.getDay()];
+let tempElement = document.querySelector("#temp")
+let cityElement = document.querySelector("#city")
+let descriptionElement = document.querySelector("#description")
 
-let hours = now.getHours();
-let minutes = now.getMinutes();
-
-let dayandtime = document.querySelector("h2");
-dayandtime.innerHTML = `${day} ${hours}:${minutes}`;
-
-function show(response) {
-  document.querySelector("h1").innerHTML = response.data.name;
-  document.querySelector("h3").innerHTML = `${Math.round(
-    response.data.main.temp
-  )}°`;
-  document.querySelector(".country").innerHTML = response.data.sys.country;
+tempElement.innerHTML = Math.round(response.data.temperature.current);
+cityElement.innerHTML= (response.data.city);
+descriptionElement.innerHTML = (response.data.condition.description);
 }
 
-function search(event) {
-  event.preventDefault();
-  let apiKey = "c8735bb7e8e2f8d8a38c7501f3cd47d3";
-  let city = document.querySelector("#location").value;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&&units=metric`;
-  axios.get(apiUrl).then(show);
-}
+let apiKey = "a8653dt9c848fab44feofb6cd970ad29";
+let apiURL = `https://api.shecodes.io/weather/v1/current?query=Berlin&key=${apiKey}&units=metric`;
 
-let form = document.querySelector("#cityForm");
-form.addEventListener("submit", search);
-
+axios.get(apiURL).then(displayTemperature);
