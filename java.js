5b4eaba2099df7b44977d31b7792cd5f15ce96e6
MirandaLee1 <134345648+MirandaLee1@users.noreply.github.com>
2023-10-26 13:17:17 +0200

    API integrations for values

--- java.js
+++ java.js
@@ -5,12 +5,24 @@ let tempElement = document.querySelector("#temp")
 let cityElement = document.querySelector("#city")
 let descriptionElement = document.querySelector("#description")
 
-tempElement.innerHTML = Math.round(response.data.temperature.current);
+let windElement = document.querySelector(".wind .value");
+let humidityElement=document.querySelector(".humidity .value");
+let pressureElement = document.querySelector(".pressure .value")
+
+
+
+
+
+tempElement.innerHTML = `${Math.round(response.data.temperature.current)}°`;
 cityElement.innerHTML= (response.data.city);
 descriptionElement.innerHTML = (response.data.condition.description);
+windElement.innerHTML = `${(response.data.wind.speed)} km/h`;
+humidityElement.innerHTML = `${(response.data.temperature.humidity)}%`;
+pressureElement.innerHTML = `${(response.data.temperature.pressure)} Pa`;
 }
 
 let apiKey = "a8653dt9c848fab44feofb6cd970ad29";
 let apiURL = `https://api.shecodes.io/weather/v1/current?query=Berlin&key=${apiKey}&units=metric`;
 
 axios.get(apiURL).then(displayTemperature);
+
