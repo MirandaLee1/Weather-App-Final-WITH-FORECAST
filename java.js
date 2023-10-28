0e82050f8918e1b2b0cf49e35165925b458a77a6
MirandaLee1 <134345648+MirandaLee1@users.noreply.github.com>
2023-10-28 16:01:01 +0200

    Added date and time

--- java.js
+++ java.js
@@ -1,8 +1,30 @@
-function displayTemperature(response) {
+function formatDate(timestamp){
+    let date = new Date (timestamp);
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let days = 
+    ["Sunday", 
+    "Monday", 
+    "Tuesday", 
+    "Wednesday", 
+    "Thursday", 
+    "Friday", 
+    "Saturday"]
 
+    let day = days[date.getDay()];
+    return `${day} ${hours}:${minutes}`;
+
+
+}
+
+
+
+function displayTemperature(response) {
+    console.log (response.data);
 
 let tempElement = document.querySelector("#temp")
 let cityElement = document.querySelector("#city")
+let dateElement = document.querySelector ("#date")
 let descriptionElement = document.querySelector("#description")
 
 let windElement = document.querySelector(".wind .value");
@@ -10,12 +32,12 @@ let humidityElement=document.querySelector(".humidity .value");
 let pressureElement = document.querySelector(".pressure .value")
 
 
-
-
-
 tempElement.innerHTML = `${Math.round(response.data.temperature.current)}°`;
 cityElement.innerHTML= (response.data.city);
+dateElement.innerHTML= formatDate(response.data.time *1000);
+
 descriptionElement.innerHTML = (response.data.condition.description);
+
 windElement.innerHTML = `${(response.data.wind.speed)} km/h`;
 humidityElement.innerHTML = `${(response.data.temperature.humidity)}%`;
 pressureElement.innerHTML = `${(response.data.temperature.pressure)} Pa`;
@@ -23,6 +45,7 @@ pressureElement.innerHTML = `${(response.data.temperature.pressure)} Pa`;
 
 let apiKey = "a8653dt9c848fab44feofb6cd970ad29";
 let apiURL = `https://api.shecodes.io/weather/v1/current?query=Berlin&key=${apiKey}&units=metric`;
-
+console.log(apiURL)
 axios.get(apiURL).then(displayTemperature);
+
 
