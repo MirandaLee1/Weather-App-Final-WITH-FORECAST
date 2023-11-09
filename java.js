814611daaca23cf16c0546d15623f92f362019a7
MirandaLee1 <134345648+MirandaLee1@users.noreply.github.com>
2023-11-09 02:27:53 +0100

    Added search engine functionality

--- java.js
+++ java.js
@@ -1,3 +1,25 @@
+function handleSearchSubmit(event){
+  event.preventDefault();
+
+  let searchInput = document.querySelector("#search-form-input")
+  let cityElement = document.querySelector("#city")
+
+  cityElement.innerHTML = searchInput.value;
+
+
+}
+
+
+let searchFormElement = document.querySelector("#searchForm")
+searchFormElement.addEventListener("submit", handleSearchSubmit); 
+
+
+
+
+
+
+
+
 function formatDate(timestamp){
     let date = new Date (timestamp);
     let hours = date.getHours();
